4d28c80ac3aa4a1bb47725e1a98847566e6fe146
Ajeet Singh <imajeet5@gmail.com>
2020-07-29 16:48:52 +0530

    added Search feature (with issue in useEffect)

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,56 +2,63 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Search = (props) => {
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState("programming");
+  const [results, setResults] = useState([]);
 
   console.log("I run with every render");
-
-  const arr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
-  const promises = [];
-  for (let el of arr) {
-    promises.push(
-      axios.get("https://en.wikipedia.org/w/api.php", {
-        params: {
-          action: "query",
-          list: "search",
-          origin: "*",
-          format: "json",
-          srsearch: el,
-        },
-      })
-    );
-  }
+  console.log(results);
 
   // We want to do the api call every single time user presses a key
   // so useEffect will run initially then every time component re-render and term changes
   useEffect(() => {
     const search = async () => {
-      //   const result = await axios.get("https://en.wikipedia.org/w/api.php", {
-      //     params: {
-      //       action: "query",
-      //       list: "search",
-      //       origin: "*",
-      //       format: "json",
-      //       srsearch: term,
-      //     },
-      //   });
-      const result = [];
-      for (let i = 0; i < promises.length; i++) {
-        const r = await promises[i];
-        result.push(r.data.query.search[0].title);
-      }
-      // promises.forEach(async promise => {
-      //     const r = await promise
-      //     result.push(r.data.query.search[0].title)
-      // })
-        console.log(result);
-      //   const result = await Promise.all(promises);
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
 
-      //   console.log(result.map((r) => r.data.query.search[0].title));
-      console.log(term);
+      if (data.query) {
+        setResults(data.query.search);
+      }
     };
-    search();
-  }, [term]);
+
+    if (term && !results.length) {
+      search();
+    } else {
+      const timerRef = setTimeout(() => {
+        if (term) search();
+      }, 1000);
+
+      return () => {
+        console.log("Clean up");
+        clearTimeout(timerRef);
+      };
+    }
+  }, [term, results.length]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div className="item" key={result.pageid}>
+        <div className="right floated content">
+          <a
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            className="ui button"
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
@@ -65,6 +72,7 @@ const Search = (props) => {
           ></input>
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
